847e01508718bfdec79f39ab11310c43d5aa8e76
rkrishn7 <ro@rohank.me>
2023-03-14 10:46:47 -0700

    vga buffer abstraction

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
@@ -12,15 +14,7 @@ static HELLO_WORLD: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    let light_cyan_color = 0xb;
-
-    for (i, &byte) in HELLO_WORLD.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = light_cyan_color;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
